5d1fc1495bd7ec247e6b53abd1ad5b1dafa9e028
nadiannis <annisanadianeyla@gmail.com>
2023-12-05 16:43:14 +0700

    feat: create send_email program

---
+++ send_email/go.mod
@@ -0,0 +1,5 @@
+module github.com/nadiannis/go-mini-programs/tree/main/send_email
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ send_email/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"mime/quotedprintable"
+	"net/mail"
+	"net/smtp"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type smtpServer struct {
+	host string
+	port int
+}
+
+type sender struct {
+	email    string
+	password string
+	smtpServer
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+
+	smtpPort, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	sender := newSender(os.Getenv("SENDER_EMAIL"), os.Getenv("SENDER_PASSWORD"), os.Getenv("SMTP_HOST"), smtpPort)
+
+	emails := getInput(reader, ">>> Recipient email (separate with ; if more than one):\n")
+	recipientEmails := strings.Split(emails, ";")
+	for _, recipientEmail := range recipientEmails {
+		if !isValidEmail(recipientEmail) {
+			log.Fatalln("Invalid email:", recipientEmail)
+		}
+	}
+
+	subject := getInput(reader, "\n>>> Subject:\n")
+	body := getMultilineInput(reader, "\n>>> Body (enter :s to save):\n")
+
+	fmt.Println("\nSending email...")
+	if err := sender.sendEmail(recipientEmails, subject, body); err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Email sent successfully")
+}
+
+func (s *sender) sendEmail(to []string, subject, body string) error {
+	serverAddress := fmt.Sprintf("%s:%d", s.host, s.port)
+	auth := smtp.PlainAuth("", s.email, s.password, s.host)
+
+	body = s.writeEmail(to, subject, body)
+	if err := smtp.SendMail(serverAddress, auth, s.email, to, []byte(body)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *sender) writeEmail(to []string, subject, body string) string {
+	var message string
+	var encodedBody bytes.Buffer
+	delimiter := fmt.Sprintf("**=mail%d", time.Now().UnixNano())
+
+	encodeMessage(body, &encodedBody)
+
+	message += fmt.Sprintf("From: %s\r\n", s.email)
+	message += fmt.Sprintf("To: %s\r\n", strings.Join(to, ";"))
+
+	message += fmt.Sprintf("Subject: %s\r\n", subject)
+	message += "MIME-Version: 1.0\r\n"
+	message += fmt.Sprintf("Content-Type: multipart/alternative; boundary=\"%s\"\r\n", delimiter)
+
+	message += fmt.Sprintf("--%s\r\n", delimiter)
+	message += "Content-Transfer-Encoding: quoted-printable\r\n"
+	message += fmt.Sprintf("Content-Type: %s; charset=\"utf-8\"\r\n", "text/plain")
+	message += "Content-Disposition: inline\r\n"
+	message += fmt.Sprintf("%s\r\n", encodedBody.String())
+
+	return message
+}
+
+func getInput(reader *bufio.Reader, prompt string) string {
+	if len(prompt) != 0 {
+		fmt.Print(prompt)
+	}
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return strings.TrimSpace(input)
+}
+
+func getMultilineInput(reader *bufio.Reader, prompt string) string {
+	if len(prompt) != 0 {
+		fmt.Print(prompt)
+	}
+
+	var lines []string
+	for {
+		line := getInput(reader, "")
+		if strings.ToLower(line) == ":s" {
+			fmt.Println("Saved!")
+			break
+		}
+		lines = append(lines, line)
+	}
+	return strings.Join(lines, "\n")
+}
+
+func newSender(email, password, host string, port int) *sender {
+	return &sender{
+		email:    email,
+		password: password,
+		smtpServer: smtpServer{
+			host: host,
+			port: port,
+		},
+	}
+}
+
+func encodeMessage(body string, encodedBody *bytes.Buffer) {
+	writer := quotedprintable.NewWriter(encodedBody)
+	if _, err := writer.Write([]byte(body)); err != nil {
+		log.Fatalln(err)
+	}
+	defer writer.Close()
+}
+
+func isValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
